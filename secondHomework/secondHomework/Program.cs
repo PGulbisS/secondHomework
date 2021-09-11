c9d12b71c6658250ca8a65066618d2fc4e537b3f
Peteris Gulbis <gulbis.storhs@gmail.com>
2021-09-11 13:31:08 +0300

    learning

--- secondHomework/secondHomework/Program.cs
+++ secondHomework/secondHomework/Program.cs
@@ -9,31 +9,75 @@ namespace secondHomework
             //Console.WriteLine("Hello, what is your name?");
             //string username = Console.ReadLine();
             //Console.WriteLine("Hi," + username);
-            ////---------------------------
+            //---------------------------------------------------
 
-            //Console.WriteLine("Hi, how old are you?");
+            //Console.WriteLine("How old are you?");
             //string ageText = Console.ReadLine();
             //int age = int.Parse(ageText);
             //int nextYearAge = age + 1;
             //Console.WriteLine("Next year you will be " + nextYearAge);
-            ////-------------------------------------
+            //////---------------------------------------------------
+
+
+            //Console.WriteLine("Please, give me one number");
+            //string userNumber1 = Console.ReadLine();
+            //Console.WriteLine("Now give me diffrent one, I will show you which is bigger");
+            //string userNumber2 = Console.ReadLine();
+            //int maxNumber1 = int.Parse(userNumber1);
+            //int maxNumber2 = int.Parse(userNumber2);
+
+            //int maxResult = Math.Max(maxNumber1, maxNumber2);
+            //Console.WriteLine(maxResult + "This number is the bigger one");
+
+
+
 
             //int maxNumber1 = 65;
             //int maxNumber2 = 120;
             //int maxResult = Math.Max(maxNumber1, maxNumber2);
             //Console.WriteLine(maxResult);
-            //--------------------------------------------
+            //-------------------------------------------------------------
+
+
+            //Console.WriteLine("And again give me please one number");
+            //string userNumber3 = Console.ReadLine();
+            //Console.WriteLine("Now can you give me another, i will show you which is smaller");
+            //string userNumber4 = Console.ReadLine();
+            //int minNumber3 = int.Parse(userNumber3);
+            //int minNumber4 = int.Parse(userNumber4);
+            //int minResult = Math.Min(minNumber3, minNumber4);
+            //Console.WriteLine(minResult + "This number is the smallest");
+
 
             //int minNumber3 = 35;
             //int minNumber4 = 90;
             //int minResult = Math.Min(minNumber3, minNumber4);
             //Console.WriteLine(minResult);
-            //----------------------------------------
+            //----------------------------------------------------------------------
+
+
+            //Console.WriteLine("Now can you give me some random number?");
+            //string userRandom1 = Console.ReadLine();
+            //Console.WriteLine("And another one, i will show you how much it can divide");
+            //string userRandom2 = Console.ReadLine();
+            //int random1 = int.Parse(userRandom1);
+            //int random2 = int.Parse(userRandom2);
+            //int divisionResult = random1 % random2;
+            //Console.WriteLine(divisionResult);
+            //Console.WriteLine("This is the result of dividing");
 
 
 
 
-                
+            //int divisionreminder1 = 100;
+            //int divisionreminder2 = 30;
+            //int divisionReminderResult = divisionreminder1 % divisionreminder2;
+            //Console.WriteLine(divisionReminderResult);
+
+            //------------------------------------------------------------------------
+
+
+
 
         }
     }
