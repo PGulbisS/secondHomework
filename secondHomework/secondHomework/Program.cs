29456c5bbe45344feb85fc9af576b4912507c591
Peteris Gulbis <gulbis.storhs@gmail.com>
2021-09-11 12:27:11 +0300

    trying to learn

---
+++ secondHomework/secondHomework/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace secondHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, what is your name?");
+            //string username = Console.ReadLine();
+            //Console.WriteLine("Hi," + username);
+            ////---------------------------
+
+            //Console.WriteLine("Hi, how old are you?");
+            //string ageText = Console.ReadLine();
+            //int age = int.Parse(ageText);
+            //int nextYearAge = age + 1;
+            //Console.WriteLine("Next year you will be " + nextYearAge);
+            ////-------------------------------------
+
+            //int maxNumber1 = 65;
+            //int maxNumber2 = 120;
+            //int maxResult = Math.Max(maxNumber1, maxNumber2);
+            //Console.WriteLine(maxResult);
+            //--------------------------------------------
+
+            //int minNumber3 = 35;
+            //int minNumber4 = 90;
+            //int minResult = Math.Min(minNumber3, minNumber4);
+            //Console.WriteLine(minResult);
+            //----------------------------------------
+
+
+
+
+                
+
+        }
+    }
+}
